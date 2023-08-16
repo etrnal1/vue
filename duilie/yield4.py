541839083bd5bd716919ce9caec1e325c0b4b2c8
test <te>
2023-08-16 13:56:46 +0800

    yield改变函数的计算规则,只有for循环或next才会调用触发

--- duilie/yield4.py
+++ duilie/yield4.py
@@ -2,13 +2,13 @@
 # yield处理管道和工作流相关的各种数据时,生成器特别有效
 import pathlib
 import re
-
+#只关心路径名
 def get_paths(topdir,pattern):
     pass
     for path in pathlib.Path(topdir).rglob(pattern):
         if path.exists():
             yield path
-
+#只关心打开文件
 def get_files(paths):
     pass
     for path in paths:
@@ -35,3 +35,4 @@ files=get_files(paths)
 lines=get_lines(files)
 comments=get_comments(lines)
 print_matching(comments,'ts')
+#生成器改变了函数的计算规则,通常当应用一个生成器函数时,它的执行将延迟,直到其他代码对他显示调用next()或佛通过for循环
