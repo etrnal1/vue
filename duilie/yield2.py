a035141d66ca5bcb4b10497174a0add3c9c2f156
test <te>
2023-08-16 11:11:54 +0800

    yield from countup(n) 不必亲自驱动迭代

--- duilie/yield2.py
+++ duilie/yield2.py
@@ -16,10 +16,34 @@ for n in c:
 
 
 class countdown:
-    def __init__(self,start)
+    def __init__(self,start):
         self.start=start
     def __iter__(self):  #每次迭代是,__iter__()都会创建一个新的生生成器
         n=self.start
         while n>0:
             yield n
             n-=1
+
+# 生成器委托
+##生成器的基本特性，涉及到yield不会自动执行,除非调用next()和for循环
+
+def countup(stop):
+    n=1
+    while n<=stop:
+        yield n
+        n+=1
+
+def countdown(start):
+    n=start
+    while n>0:
+        yield n
+        n-=1
+
+def up_and_down(n):
+    yield from countup(n) 
+    yield from countdown(n)
+
+#yield from 有效地将迭代过程委托给外部迭代
+
+for x in up_and_down(5):
+    print(x,end='')
