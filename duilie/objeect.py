28fd69dd660e9b998d1e2a1ff5363f579da264bb
test <te>
2023-08-16 09:34:11 +0800

    items 里面添加函数，方法，对象

--- duilie/objeect.py
+++ duilie/objeect.py
@@ -7,3 +7,17 @@ print(repr(d))
 
 print(f'th date is: {d!r}')
 #在字符串格式化中 将!r后缀添加到值来生成repr值,以区别常规的字符串转换
+#头等对象
+#python 中的所有对象都是头等对象,对象可以被当做变量存储，
+
+items ={
+    'number':42,
+    'text':"hello world"
+}
+print(items)
+items['func'] =abs
+import math
+items['mod'] =math
+nums =[1,2,3,4]
+items['append'] = nums.append
+print(items)
